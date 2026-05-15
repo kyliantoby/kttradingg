c82f85c567310edc4cd14c77846f944c33ee9502
kyliantoby <kttradingcards@gmail.com>
2026-05-16 00:13:34 +0200

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>KT Trading Cards</h1>
+      <p>Webshop is live 🚀</p>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
